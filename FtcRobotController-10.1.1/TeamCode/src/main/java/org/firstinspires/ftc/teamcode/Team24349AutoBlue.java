d222f7b223f67613be4a119368c5b8c4ca291de0
Ms. Mills <danielle.mills@nisd.net>
2025-01-09 13:49:16 -0600

    Updated Auto

--- FtcRobotController-10.1.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team24349AutoBlue.java
+++ FtcRobotController-10.1.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team24349AutoBlue.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @Autonomous (name="Team24349Auto1ParkingBlue", group="24349")
 public class Team24349AutoBlue extends LinearOpMode{
 
-    Team24349HM robot = new Team24349HM();
+    MightyMouseHM robot = new MightyMouseHM();
     ElapsedTime Time = new ElapsedTime();
     double multy = 0.3;
 
@@ -15,20 +15,27 @@ public class Team24349AutoBlue extends LinearOpMode{
     public void runOpMode() {
         robot.Map(hardwareMap);
         waitForStart();
-        // when straffing, both wheels spin inwards
-        robot.DriveRightFront.setPower(-1);
-        robot.DriveRightBack.setPower(1);
-        robot.DriveLeftBack.setPower(1);
-        robot.DriveLeftFront.setPower(-1);
 
-        sleep(1600);
-
-        robot.DriveRightFront.setPower(0);
-        robot.DriveRightBack.setPower(0);
-        robot.DriveLeftBack.setPower(0);
-        robot.DriveLeftFront.setPower(0);
+        robot.driveFrontRight.setPower(-1);
+        robot.driveFrontLeft.setPower(0.9);
 
 
+        sleep(1700);
+
+        robot.driveFrontLeft.setPower(0);
+        robot.driveFrontRight.setPower(0.5);
+
+        sleep(1400);
+
+        robot.driveFrontLeft.setPower(-0.9);
+        robot.driveFrontRight.setPower(1);
+
+        sleep(200);
+
+        robot.armRotator.setPower(0.8);
+        robot.armRotator2.setPower(0.8);
+
+        sleep(600);
 
         // write code here
 
