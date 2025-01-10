8e6834a09dd965c907678973386a3710943d2a40
Ms. Mills <danielle.mills@nisd.net>
2025-01-10 15:58:42 -0600

    Update Team24349AutoBlue.java

--- FtcRobotController-10.1.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team24349AutoBlue.java
+++ FtcRobotController-10.1.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team24349AutoBlue.java
@@ -16,8 +16,8 @@ public class Team24349AutoBlue extends LinearOpMode{
         robot.Map(hardwareMap);
         waitForStart();
 
-        robot.driveFrontRight.setPower(-1);
-        robot.driveFrontLeft.setPower(0.9);
+        robot.driveFrontRight.setPower(-0.85);
+        robot.driveFrontLeft.setPower(1);
 
 
         sleep(900);
@@ -25,17 +25,44 @@ public class Team24349AutoBlue extends LinearOpMode{
         robot.driveFrontLeft.setPower(0);
         robot.driveFrontRight.setPower(0.5);
 
-        sleep(1200);
+        sleep(850);
 
-        robot.driveFrontLeft.setPower(-0.9);
-        robot.driveFrontRight.setPower(1);
+        robot.driveFrontLeft.setPower(0.9);
+        robot.driveFrontRight.setPower(-0.85);
+
+        sleep(890);
+
+        robot.driveFrontLeft.setPower(0);
+        robot.driveFrontRight.setPower(0.4);
+
+        sleep(400);
+
+        robot.armRotator.setPower(0.3);
+        robot.armRotator2.setPower(0.3);
 
         sleep(200);
 
-        robot.armRotator.setPower(0.8);
-        robot.armRotator2.setPower(0.8);
 
-        sleep(600);
+        robot.driveFrontLeft.setPower(0);
+        robot.driveFrontRight.setPower(0);
+
+        sleep(1000);
+
+        robot.driveFrontLeft.setPower(1);
+        robot.driveFrontRight.setPower(-0.8);
+
+        sleep(850);
+
+        robot.armRotator.setPower(-0.3);
+        robot.armRotator2.setPower(-0.3);
+
+        sleep(200);
+
+        robot.wrist.setPower(0.9);
+
+        sleep(500);
+
+        robot.claw.setPosition(1);
 
         // write code here
 
