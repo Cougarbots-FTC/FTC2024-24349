6d2b91f12b5ad0d236683f97c23218eb46d2c4fd
SanjanaVaranasi <varanasisanjana15@gmail.com>
2025-01-31 08:51:22 -0600

    updated pidf

--- FtcRobotController-10.1.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MightyMouseTeleOp.java
+++ FtcRobotController-10.1.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MightyMouseTeleOp.java
@@ -9,14 +9,14 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class MightyMouseTeleOp extends LinearOpMode {
     MightyMouseHM robot = new MightyMouseHM();
     private PIDFArm pidf;
-    private PIDFArm pidf2;
+    //private PIDFArm pidf2;
 
     @Override
     public void runOpMode() {
         // initializing the hardware map for the robot
         robot.Map(hardwareMap);
-        pidf = new PIDFArm(robot.armRotator,0.007,0,0.0005,0,3);
-        pidf2 = new PIDFArm(robot.armRotator2,0.007,0,0.0005,0,2);
+        pidf = new PIDFArm(robot.armRotator,0,0,0,0,3);
+        //pidf2 = new PIDFArm(robot.armRotator2,0,0,0,0,2);
 
 
         // telemetry stores positions and other values that are assigned
@@ -70,19 +70,25 @@ public class MightyMouseTeleOp extends LinearOpMode {
             // this is for lifting the arm
             // by using gamepad 2, this is for when we press start B
             double armRotatorUp = gamepad2.left_trigger;
-            int num = 100*(int)(armRotatorUp/0.01);
             if (armRotatorUp > 0.1) {
-                pidf.setSetpoint(pidf.getCurrentPosition()+num); // Set encoder position to 1000
-                pidf2.setSetpoint(pidf2.getCurrentPosition()+num); // Set encoder position to 2000
+                pidf.setPower(armRotatorUp); // Set encoder position to 1000
+                //pidf2.setPower(armRotatorUp); // Set encoder position to 1000
+            }
+            else {
+                pidf.setPower(0); // Set encoder position to 1000
+                //pidf2.setPower(0); // Set encoder position to 1000
             }
             // this is for lowering the arm
             double armRotatorDown = gamepad2.right_trigger;
-            int val = 100*(int)(armRotatorDown/0.01);
 
             //int val = Math.round(armRotatorDown)*10;
             if (armRotatorDown > 0.1) {
-                pidf.setSetpoint(pidf.getCurrentPosition()-val); // Set encoder position to 1000
-                pidf2.setSetpoint(pidf2.getCurrentPosition()-val); // Set encoder position to 2000
+                pidf.setPower(-1 * armRotatorDown); // Set encoder position to 1000
+                //pidf2.setPower(-1 * armRotatorDown); // Set encoder position to 1000
+            }
+            else {
+                pidf.setPower(0); // Set encoder position to 1000
+                //pidf2.setPower(0); // Set encoder position to 1000
             }
 
             //pidf code to set arm to a certain position
@@ -90,21 +96,21 @@ public class MightyMouseTeleOp extends LinearOpMode {
             // when y is pressed the arm moves to grab a specimen from the wall
             if (gamepad2.x) {
                 pidf.setSetpoint(290); // Set encoder position to 1000
-                pidf2.setSetpoint(289); // Set encoder position to 2000
+                //pidf2.setSetpoint(289); // Set encoder position to 2000
 
             } else if (gamepad2.y) {
                 pidf.setSetpoint(140); // Set encoder position to 2000
-                pidf2.setSetpoint(139); // Set encoder position to 2000
+                //pidf2.setSetpoint(139); // Set encoder position to 2000
 
             }
             pidf.loop();
-            pidf2.loop();
+            //pidf2.loop();
 
             // Telemetry for debugging
             telemetry.addData("Target1 Position", pidf.getSetpoint());
-            telemetry.addData("Target2 Position", pidf2.getSetpoint());
+            //telemetry.addData("Target2 Position", pidf2.getSetpoint());
             telemetry.addData("Current1 Position", robot.armRotator.getCurrentPosition());
-            telemetry.addData("Current2 Position", robot.armRotator2.getCurrentPosition());
+            //telemetry.addData("Current2 Position", robot.armRotator2.getCurrentPosition());
             telemetry.update();
 
             // this is to open and close the claw
