c21bc7de508af0600d81d1c5d2138b5719defd8c
SanjanaVaranasi <varanasisanjana15@gmail.com>
2025-02-01 09:48:32 -0600

    auto update

--- FtcRobotController-10.1.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/specimen.java
+++ FtcRobotController-10.1.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/specimen.java
@@ -17,35 +17,34 @@ public class specimen extends LinearOpMode{
         waitForStart();
 
         //moves forward to the rod
-        robot.driveFrontLeft.setPower(0.85);
-        robot.driveFrontRight.setPower(-0.9);
+        robot.driveFrontLeft.setPower(0.9);
+        robot.driveFrontRight.setPower(-1);
         robot.claw.setPosition(0);
-        sleep(750);
+        sleep(1000);
+
+        robot.driveFrontLeft.setPower(-0.85);
+        robot.driveFrontRight.setPower(0.9);
+        robot.claw.setPosition(0);
+        sleep(40);
 
         //moves the arm slightly upwards
         robot.driveFrontLeft.setPower(0);
         robot.driveFrontRight.setPower(0);
         robot.armRotator.setPower(0.4);
-        //robot.armRotator2.setPower(0.4);
-        sleep(300);
+        robot.armRotator2.setPower(0.4);
+        sleep(400);
 
         //moves the wrist upwards so that the rod and clip line up
         robot.wrist.setPower(1);
         robot.armRotator.setPower(0);
-        //robot.armRotator2.setPower(0);
+        robot.armRotator2.setPower(0);
         sleep(500);
 
         //moves both the wrist and arm down to clip the specimen
         robot.wrist.setPower(1);
         robot.armRotator.setPower(-0.75);
-        //robot.armRotator2.setPower(-0.75);
+        robot.armRotator2.setPower(-0.75);
         sleep(475);
 
-        //opens the claw after hooking the specimen
-        robot.claw.setPosition(1);
-        robot.wrist.setPower(0);
-        robot.armRotator.setPower(0);
-        //robot.armRotator2.setPower(0);
-        sleep(300);
     }
 }
